99ea926d4c4b132034fd8403e7db4056c31fb857
pedro prado <pedrohhprado@gmail.com>
2023-11-27 13:11:38 -0300

    seleção de candidatos baseado em salario

---
+++ src/Candidatura/ProcessoSeletivo.java
@@ -0,0 +1,49 @@
+package Candidatura;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ProcessoSeletivo {
+    public static void main(String[] args) {
+        System.out.println("Processo seletivo");
+
+    }
+
+    static void selecaoCandidatos(){
+        String [] candidatos = {"PEDRO", "RANIA", "LUCAS", "MATHEUS", "IAGO", "KAMYLA", "VITOR", "AMANDA", "RAIRA", "JOÃO"};
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+
+        while(candidatosSelecionados < 5){
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + "solicitou esse salario" + salarioPretendido);
+            if(salarioBase>+salarioPretendido){
+                System.out.println("o candidato" + candidato + "foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatoAtual++;
+
+        }
+
+    };
+
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
+
+
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido) {
+            System.out.println("Ligar para o candidato");
+        } else if (salarioBase == salarioPretendido) {
+            System.out.println("Ligar para candidato com contra proposta");
+        } else {
+            System.out.println("Aguardando os demais candidatos");
+        }
+    }
+}
